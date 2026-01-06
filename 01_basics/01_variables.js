ef484a138c6048e0b37b737fa4ef558016ce2750
Gaurav Agrawal <154917681+2005gaurav@users.noreply.github.com>
2026-01-06 15:46:15 +0000

    #

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+
+const accountId = 14453;
+// const value ko reassign nhi krta ha
+let accountEmail = "gaurav@gmail.com";
+// let variable can reassign value
+var accountPassword = "12345"
+
+
+
+accountCity="jaipur"
+// in js we can assign value without declaring variable
+
+let accountState;
+// ya undefined hoga kyuki issma kohi value nhi diya ha
+
+
+// changing value 
+accountEmail = "no@gmail.com";
+accountPassword="45678";
+accountCity="Bengaluru"
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+// ya console.table multiple varaible ko 1 sath print krna ko use hota ha
+
+
