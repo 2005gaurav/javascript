4098c0b5cf040e4b2888903951ac9f104ff2e6c3
Gaurav Agrawal <154917681+2005gaurav@users.noreply.github.com>
2026-01-08 19:53:07 +0000

    numbers and math

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// NORAML COMPARSION CHECK 
+console.log(2>1);
+console.log(2<1);
+console.log(2==2);
+
+// DONT DO THESE TYPE OF COMPARSION 
+console.log(null>0);
+console.log(null<0);
+console.log(null==0)
+
+// COMPARSION CHECKING 
+console.log("2"==2);
+// iska output --> true ayga
+// idhar string "2"==2 number 2 ka ha kinhi
+// idhar STRICT COMPARSION CHECK NHI HUA HA == convert krdeta ha datatype kyuki dono datatype same ho
+
+// STRICT COMPARISON
+console.log("2"===2)
+// iska ouput --> false 
+// kyuki === equal to do strict comparsion
+// ya dono value ka datatype ko check krta ha agar same hua tavi output-->true deta ha
+// nhi to  false deta ha
+
